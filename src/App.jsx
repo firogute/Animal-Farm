0102db075ad425a2d6aea720a121dbed819b075e
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-04-12 14:45:17 +0300

    feat: Implement animal search with localStorage support

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+
+import Animal from "./component/Animal";
+const userSearchAnimal = () => {
+  const [animals, setAnimals] = useState([]);
+  const handleChange = async (q) => {
+    const response = await fetch(
+      "http://localhost:8080/?" + new URLSearchParams({ q })
+    );
+    const data = await response.json();
+    setAnimals(data);
+    localStorage.setItem("lastQuery", q);
+  };
+
+  useEffect(() => {
+    const lastQuery = localStorage.getItem("lastQuery");
+    handleChange(lastQuery);
+  }, []);
+  return { handleChange, animals };
+};
 
 function App() {
-  return <div></div>;
+  const { handleChange, animals } = userSearchAnimal();
+  return (
+    <div style={{ padding: "6rem" }}>
+      <h1>Animal Farm</h1>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        placeholder="Search....."
+        onChange={(e) => handleChange(e.target.value)}
+      />
+
+      <ul>
+        {animals.map((animal) => {
+          return <Animal key={animal.id} {...animal} />;
+        })}
+
+        {animals.length == 0 && "No animals found"}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
